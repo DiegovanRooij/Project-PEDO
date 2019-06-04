4fbffe6f7ce52889ef0dc733d38354de2098517f
cicilia <ciciliapham@gmail.com>
2019-06-04 21:51:53 +0200

    updates

--- map/Leerling_Check.php
+++ map/Leerling_Check.php
@@ -21,11 +21,11 @@ $WachtWoord = $_POST['txtPassword'];
 #Check of de gebruiker een leerling is of een leraar!
 $Leerling_Leraar = "SELECT Rights from gebruikers where Gebruikersnaam = '$PSnummer' ";
 #Checks if you have the rights or not
-$NotAdmin = 1;
+$NotAdmin = 0;
 
 if ($Check = mysqli_query($conn, $Leerling_Leraar)) {
     while ($Checks = mysqli_fetch_row($Check)) {
-        $Admin = $Checks[1];
+        $NotAdmin = $Checks[0];
     }
 }
 
@@ -33,15 +33,15 @@ if ($Check = mysqli_query($conn, $Leerling_Leraar)) {
 $Sql = "SELECT * FROM gebruikers WHERE Gebruikersnaam = '$PSnummer' AND Wachtwoord = '$WachtWoord' ";
 $Result = mysqli_query($conn, $Sql);
 
-if ($Admin == 1) {
+if ($Admin == 0) {
     if (mysqli_num_rows($Result) > 0) {
         while ($Row = mysqli_fetch_array($Result,  MYSQLI_NUM)) {
-            header("Refresh: 1; url=./Home/index.html");
+            header("Refresh: 0; url=../Home/index.html");
         }
     } else {
         echo "Jouw login en/of wachtwoord klopt niet";
     }
 }
 else{
-    header("Refresh: 0; url=./Admin.html");
+    header("refresh:0;url=../Home/index.html");
 }
